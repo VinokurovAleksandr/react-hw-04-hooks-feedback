fc009f3261d50686caa98a18148c654863107d58
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2023-03-29 21:34:36 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import React from 'react';
+
+
+
+
+export default class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <form>
+        <label>
+          Login
+           <input type='text' name='login' />
+        </label>
+        <label>
+          Password
+          <input type='text' name='password' />
+        </label>
+       <button type="button">Login</button>
+          
+      
+    </form>
+    );
+  }
+}
+ 
+// export default App;
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
